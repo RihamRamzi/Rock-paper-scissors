0970e02f9f54ba89c2d62f275f6d0a8456e3cbbb
RihamRamzi <rihamsoftware@gmail.com>
2024-11-05 06:11:18 +0530

    completed the play game function
    1.moved the play round function and the score
    and round variables inside the play game function
    
    2.added for loop for playround runs 5 rounds
    then added an if statement that decides the winner
    of the game

--- script.js
+++ script.js
@@ -11,49 +11,66 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  return (userChoice = prompt("Select Rock, Paper or Scissors").toLowerCase());
-}
-
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    return `Draw!`;
-  } else if (humanChoice === "rock") {
-    if (computerChoice === "scissors") {
-      humanScore++;
-      return `Rock smashes scissors, YOU WIN!`;
-    } else if (computerChoice === "paper") {
-      computerScore++;
-      return `Paper crushed the Rock, YOU LOOSE!`;
-    }
-  } else if (humanChoice === "paper") {
-    if (computerChoice === "rock") {
-      humanScore++;
-      return `Paper crushed the Rock, YOU WIN!`;
-    } else if (computerChoice === "scissors") {
-      computerScore++;
-      return `Scissors sliced the paper, YOU LOOSE!`;
-    }
-  } else if (humanChoice === "scissors") {
-    if (computerChoice === "paper") {
-      humanScore++;
-      return `Scissors sliced the paper, YOU WIN`;
-    } else if (computerChoice === "rock") {
-      computerScore++;
-      return `Rock smashes scissors, YOU LOOSE!`;
-    }
+  let userChoice = prompt("Select Rock, Paper or Scissors");
+  if (userChoice == null) {
+    alert`you exit the round`;
+  } else {
+    return userChoice.toLowerCase();
   }
 }
 
-let rounds = 5;
-let humanScore = 0;
-let computerScore = 0;
-
 function playGame() {
-  let humanChoice = getHumanChoice();
-  let computerChoice = getComputerChoice();
-  console.log(humanChoice);
+  let rounds = 5;
+  let humanScore = 0;
+  let computerScore = 0;
 
-  console.log(playRound(humanChoice, computerChoice));
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      return `Draw!`;
+    } else if (humanChoice === "rock") {
+      if (computerChoice === "scissors") {
+        humanScore++;
+        return `Rock smashes scissors, YOU WIN!`;
+      } else if (computerChoice === "paper") {
+        computerScore++;
+        return `Paper crushed the Rock, YOU LOOSE!`;
+      }
+    } else if (humanChoice === "paper") {
+      if (computerChoice === "rock") {
+        humanScore++;
+        return `Paper crushed the Rock, YOU WIN!`;
+      } else if (computerChoice === "scissors") {
+        computerScore++;
+        return `Scissors sliced the paper, YOU LOOSE!`;
+      }
+    } else if (humanChoice === "scissors") {
+      if (computerChoice === "paper") {
+        humanScore++;
+        return `Scissors sliced the paper, YOU WIN`;
+      } else if (computerChoice === "rock") {
+        computerScore++;
+        return `Rock smashes scissors, YOU LOOSE!`;
+      }
+    }
+  }
+
+  for (i = 0; i < rounds; i++) {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    console.log(`opponent choose ${computerChoice}`);
+
+    console.log(playRound(humanChoice, computerChoice));
+  }
+  console.log(`Your won ${humanScore} rounds`);
+  console.log(`Your opponent won ${computerScore} rounds`);
+
+  if (humanScore === computerScore) {
+    console.log(`The game is a Draw!`);
+  } else if (humanScore > computerScore) {
+    console.log(`You won the game!`);
+  } else {
+    console.log(`You lost the game!`);
+  }
 }
 
-for (i = 0; i < rounds; i++) {}
+playGame();
