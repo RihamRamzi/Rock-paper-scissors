ddf7a5d595adc764f680412852c93770fd542706
RihamRamzi <rihamsoftware@gmail.com>
2024-11-08 08:03:43 +0530

    i made a mistake by moving the playround function into the play game completely now corrected it

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const randomNum = Math.floor(Math.random() * 3) + 1;
 
@@ -20,39 +23,7 @@ function getHumanChoice() {
 }
 
 function playGame() {
-  let rounds = 0;
-  let humanScore = 0;
-  let computerScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-      return `Draw!`;
-    } else if (humanChoice === "rock") {
-      if (computerChoice === "scissors") {
-        humanScore++;
-        return `Rock smashes scissors, YOU WIN!`;
-      } else if (computerChoice === "paper") {
-        computerScore++;
-        return `Paper crushed the Rock, YOU LOOSE!`;
-      }
-    } else if (humanChoice === "paper") {
-      if (computerChoice === "rock") {
-        humanScore++;
-        return `Paper crushed the Rock, YOU WIN!`;
-      } else if (computerChoice === "scissors") {
-        computerScore++;
-        return `Scissors sliced the paper, YOU LOOSE!`;
-      }
-    } else if (humanChoice === "scissors") {
-      if (computerChoice === "paper") {
-        humanScore++;
-        return `Scissors sliced the paper, YOU WIN`;
-      } else if (computerChoice === "rock") {
-        computerScore++;
-        return `Rock smashes scissors, YOU LOOSE!`;
-      }
-    }
-  }
+  let rounds = 5;
 
   for (i = 0; i < rounds; i++) {
     let humanChoice = getHumanChoice();
@@ -73,8 +44,34 @@ function playGame() {
   }
 }
 
-playGame();
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    return `Draw!`;
+  } else if (humanChoice === "rock") {
+    if (computerChoice === "scissors") {
+      humanScore++;
+      return `Rock smashes scissors, YOU WIN!`;
+    } else if (computerChoice === "paper") {
+      computerScore++;
+      return `Paper crushed the Rock, YOU LOOSE!`;
+    }
+  } else if (humanChoice === "paper") {
+    if (computerChoice === "rock") {
+      humanScore++;
+      return `Paper crushed the Rock, YOU WIN!`;
+    } else if (computerChoice === "scissors") {
+      computerScore++;
+      return `Scissors sliced the paper, YOU LOOSE!`;
+    }
+  } else if (humanChoice === "scissors") {
+    if (computerChoice === "paper") {
+      humanScore++;
+      return `Scissors sliced the paper, YOU WIN`;
+    } else if (computerChoice === "rock") {
+      computerScore++;
+      return `Rock smashes scissors, YOU LOOSE!`;
+    }
+  }
+}
 
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissors = document.querySelector("#scissors");
+playGame();
