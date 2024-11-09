daf887cdc9e7597e2852fcd894427ad4fb7b019a
RihamRamzi <rihamsoftware@gmail.com>
2024-11-09 09:41:23 +0530

    Made changes on 3 files html,css and js
    1. HTML & css = added player score and opponent
    score display and added css to it
    
    2.js =  player score and opponent score value
    update function was added to the play round
    function now we can see the player score and
    opponen score increase in the web page

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ function getComputerChoice() {
 }
 
 function playGame() {
-  let rounds = 5;
-
   for (i = 0; i < rounds; i++) {
     let humanChoice = getHumanChoice();
     let computerChoice = getComputerChoice();
@@ -41,43 +39,54 @@ function playRound(humanChoice, computerChoice) {
   } else if (humanChoice === "rock") {
     if (computerChoice === "scissors") {
       humanScore++;
+      humanSDis.textContent = `player score: ${humanScore}`;
       return `Rock smashes scissors, YOU WIN!`;
     } else if (computerChoice === "paper") {
       computerScore++;
+      computerSDis.textContent = `opponent score: ${computerScore}`;
       return `Paper crushed the Rock, YOU LOOSE!`;
     }
   } else if (humanChoice === "paper") {
     if (computerChoice === "rock") {
       humanScore++;
+      humanSDis.textContent = `player score: ${humanScore}`;
       return `Paper crushed the Rock, YOU WIN!`;
     } else if (computerChoice === "scissors") {
       computerScore++;
+      computerSDis.textContent = `opponent score: ${computerScore}`;
       return `Scissors sliced the paper, YOU LOOSE!`;
     }
   } else if (humanChoice === "scissors") {
     if (computerChoice === "paper") {
       humanScore++;
+      humanSDis.textContent = `player score: ${humanScore}`;
       return `Scissors sliced the paper, YOU WIN`;
     } else if (computerChoice === "rock") {
       computerScore++;
+      computerSDis.textContent = `opponent score: ${computerScore}`;
       return `Rock smashes scissors, YOU LOOSE!`;
     }
   }
 }
+let rounds = 0;
 
 const display = document.querySelector(".display");
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
+const humanSDis = document.querySelector("#playerScore");
+const computerSDis = document.querySelector("#opponentScore");
 
 rock.addEventListener("click", () => {
   display.textContent = playRound(`rock`, getComputerChoice());
+  rounds++;
 });
+
 paper.addEventListener("click", () => {
   display.textContent = playRound(`paper`, getComputerChoice());
+  rounds++;
 });
 scissors.addEventListener("click", () => {
   display.textContent = playRound(`scissors`, getComputerChoice());
+  rounds++;
 });
-
-//console.log(playRound(getHumanChoice(), getComputerChoice()));
